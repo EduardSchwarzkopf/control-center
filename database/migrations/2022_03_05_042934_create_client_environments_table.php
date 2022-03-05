cdebdb0dc99587a5853bb246b73cd76775704551
Eduard Schwarzkopf <mail.eduard.schwarzkopf@gmail.com>
2022-03-05 15:57:41 +0100

    models and migration added back in

---
+++ database/migrations/2022_03_05_042934_create_client_environments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Database\Seeders\ClientEnvironmentSeeder;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('client_environments', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+        });
+
+        $seeder = new ClientEnvironmentSeeder();
+        $seeder->run();
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('client_environments');
+    }
+};
