2091a93cfb7d3ddf0b977c20c140162ff8999385
Eduard Schwarzkopf <mail.eduard.schwarzkopf@gmail.com>
2022-03-05 12:46:08 +0100

    added fillables

--- app/Models/ClientMeta.php
+++ app/Models/ClientMeta.php
@@ -9,6 +9,47 @@ class ClientMeta extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'is_active',
+        'check_interval',
+        'test_retries',
+        'retry_interval',
+        'url',
+        'backup_enabled',
+        'inodes_enabled',
+        'diskspace_enabled',
+        'email_enabled',
+        'backup_notification_enabled',
+        'backup_notification_receiver',
+        'backup_files_enabled',
+        'backup_database_enabled',
+        'backup_custom_folder',
+        'backup_sync_enabled',
+        'backup_sync_amount',
+        'backup_database_max_age',
+        'backup_database_amount',
+        'backup_files_max_age',
+        'backup_files_amount',
+        'diskspace_warn_level',
+        'inodes_warn_level',
+        'email_receiver'
+    ];
+    
+     protected $attributes = [
+        'is_active' => true,
+        'check_interval' => 900,
+        'test_retries' => 0,
+        'retry_interval' => 900,
+
+        'inodes_enabled' => false,
+        'backup_files_enabled' => false,
+        'backup_database_enabled' => false,
+        'diskspace_enabled' => false,
+        'email_enabled' => false,
+        'backup_notification_enabled' => false,
+        'backup_sync_enabled' => false
+    ];
+
     public function client()
     {
         return $this->belongsTo(Client::class);
