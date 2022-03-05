c11946eb3991c9adf9620d88dbedb277a42b6ce3
Eduard Schwarzkopf <mail.eduard.schwarzkopf@gmail.com>
2022-03-05 12:47:49 +0100

    updated table

--- database/migrations/2022_03_05_044631_create_client_metas_table.php
+++ database/migrations/2022_03_05_044631_create_client_metas_table.php
@@ -24,25 +24,31 @@ return new class extends Migration
             $table->unsignedBigInteger('client_environment_id');
             $table->foreign('client_environment_id')->references('id')->on('client_environments');
 
+            $table->boolean('is_active');
+            $table->integer('check_interval');
+            $table->integer('test_retries');
+            $table->integer('retry_interval');
+
             $table->string('url')->unique();
-            $table->boolean('backup_enabled');
             $table->boolean('inodes_enabled');
+            $table->boolean('backup_files_enabled');
+            $table->boolean('backup_database_enabled');
             $table->boolean('diskspace_enabled');
             $table->boolean('email_enabled');
             $table->boolean('backup_notification_enabled');
-            $table->string('backup_notification_receiver');
-            $table->boolean('backup_files_enabled');
-            $table->boolean('backup_database_enabled');
-            $table->string('backup_custom_folder');
-            $table->boolean('backup_sync_enabled');
-            $table->integer('backup_sync_amount');
-            $table->integer('backup_database_max_age');
-            $table->integer('backup_database_amount');
-            $table->integer('backup_files_max_age');
-            $table->integer('backup_files_amount');
-            $table->float('diskspace_warn_level');
-            $table->float('inodes_warn_level');
-            $table->string('email_receiver');
+            $table->boolean('remote_backup_enabled');
+
+            $table->string('backup_notification_receiver')->nullable();
+            $table->string('backup_custom_folder')->nullable();
+            $table->integer('backup_remote_amount')->nullable();
+            $table->integer('backup_database_max_age')->nullable();
+            $table->integer('backup_database_amount')->nullable();
+            $table->integer('backup_files_max_age')->nullable();
+            $table->integer('backup_files_amount')->nullable();
+
+            $table->float('diskspace_warn_level')->nullable();
+            $table->float('inodes_warn_level')->nullable();
+            $table->string('email_receiver')->nullable();
         });
     }
 
