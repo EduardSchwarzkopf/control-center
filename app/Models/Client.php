f36b76b4065502a2fff6e6bac9fcd846f08e6d40
Eduard Schwarzkopf <mail.eduard.schwarzkopf@gmail.com>
2022-03-05 08:55:59 +0100

    added one to one relationship

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Client extends Model
 {
     use HasFactory;
+
+    public function clientMeta()
+    {
+        return $this->hasOne(ClientMeta::class);
+    }
 }
