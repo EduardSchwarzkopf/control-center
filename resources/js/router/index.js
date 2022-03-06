4d9820b7d9679394fa514fbd326fe4d60f4b3d6c
Eduard Schwarzkopf <mail.eduard.schwarzkopf@gmail.com>
2022-03-06 18:01:13 +0100

    added route

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,13 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import CompaniesIndex from '../components/clients/ClientsIndex.vue'
+import ClientsCreate from '../components/clients/ClientsCreate.vue'
 
 const routes = [
     {
         path: '/dashboard',
         name: 'client.index',
         component: CompaniesIndex
-    }
+    },
+    {
+        path: '/clients/create',
+        name: 'clients.create',
+        component: ClientsCreate
+    },
 ];
 
 export default createRouter({
