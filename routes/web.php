f9aff1a63ed46c61ce9ae12f12627da5b560340d
Eduard Schwarzkopf <mail.eduard.schwarzkopf@gmail.com>
2022-03-07 05:33:37 +0100

    infinite loop fix

--- routes/web.php
+++ routes/web.php
@@ -21,8 +21,8 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-Route::view('/{any}', 'dashboard')
-    ->middleware(['auth'])
-    ->where('any', '.*');
-
 require __DIR__.'/auth.php';
+
+Route::view('/{any}', 'dashboard')
+    ->middleware('auth')
+    ->where('any', '.*');
