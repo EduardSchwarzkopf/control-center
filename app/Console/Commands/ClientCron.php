077ad15526b25451cf985fd3d355a80038fa2aec
Eduard Schwarzkopf <mail.eduard.schwarzkopf@gmail.com>
2022-03-10 16:22:10 +0100

    client command added

---
+++ app/Console/Commands/ClientCron.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Client;
+use App\Models\ClientOption;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+
+class ClientCron extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'client:cron';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $clientList = Client::all();
+
+        foreach($clientList as $client) {
+            $id = $client->id;
+            $clientName = $client->name;
+            $options = ClientOption::where('client_id', "=", $id)->first();
+
+            $infoText = "ClientCron: $clientName ($id)";
+
+            Log::info($infoText);
+            $this->info($infoText);
+        }
+    }
+}
