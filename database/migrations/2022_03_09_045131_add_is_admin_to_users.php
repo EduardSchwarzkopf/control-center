7c6370fcfd6890e7aca57d364da314a00a15c217
Eduard Schwarzkopf <mail.eduard.schwarzkopf@gmail.com>
2022-03-12 15:01:55 +0100

    user seeder added

--- database/migrations/2022_03_09_045131_add_is_admin_to_users.php
+++ database/migrations/2022_03_09_045131_add_is_admin_to_users.php
@@ -1,5 +1,6 @@
 <?php
 
+use Database\Seeders\UsersSeeder;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -16,6 +17,9 @@ return new class extends Migration
         Schema::table('users', function (Blueprint $table) {
             $table->boolean('is_admin')->default(false);
         });
+
+        $seeder = new UsersSeeder();
+        $seeder->run();
     }
 
     /**
