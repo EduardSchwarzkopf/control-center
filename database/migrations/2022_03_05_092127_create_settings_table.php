153c71f9541e513ff26b7ecd330dce3f93eb28ec
Eduard Schwarzkopf <mail.eduard.schwarzkopf@gmail.com>
2022-03-12 15:02:08 +0100

    settings seeder added

--- database/migrations/2022_03_05_092127_create_settings_table.php
+++ database/migrations/2022_03_05_092127_create_settings_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use Database\Seeders\SettingsSeeder;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -20,6 +21,9 @@ return new class extends Migration
             $table->string('name')->unique();
             $table->string('value');
         });
+        
+        $seeder = new SettingsSeeder();
+        $seeder->run();
     }
 
     /**
