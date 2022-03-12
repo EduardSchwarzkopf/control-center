ca7dc860e4011be1128b151de17bad4512388d9f
Eduard Schwarzkopf <mail.eduard.schwarzkopf@gmail.com>
2022-03-12 18:49:49 +0100

    autoformation

--- wrkbeat-client/api.php
+++ wrkbeat-client/api.php
@@ -4,7 +4,7 @@ require_once('./JWT.php');
 
 $token = JWT::getBearerToken();
 if ($token == null) {
-    echo json_encode(['message'=>'No token provided']);
+    echo json_encode(['message' => 'No token provided']);
 } else {
 
     $jwt = new JWT($token);
@@ -12,6 +12,5 @@ if ($token == null) {
     $infoText = "token is valid: $isValid";
 
 
-    echo json_encode(["is_valid"=>$isValid, 'postData' => $_POST ]);
-
+    echo json_encode(["is_valid" => $isValid, 'postData' => $_POST]);
 }
