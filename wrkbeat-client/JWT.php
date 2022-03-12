eb100a4dc66c0fa543e9dd24695510a205deaca3
Eduard Schwarzkopf <mail.eduard.schwarzkopf@gmail.com>
2022-03-12 18:48:23 +0100

    autoformation

--- wrkbeat-client/JWT.php
+++ wrkbeat-client/JWT.php
@@ -12,17 +12,19 @@ class JWT
     ) {
         $this->validateToken($token);
     }
-    
-    
-    public static function base64url_encode(string $data): string {
+
+
+    public static function base64url_encode(string $data): string
+    {
         return rtrim(strtr(base64_encode($data), '+/', '-_'), '=');
     }
 
-    public static function base64url_decode(string $data):string {
-    return base64_decode(str_pad(strtr($data, '-_', '+/'), strlen($data) % 4, '=', STR_PAD_RIGHT));
-    }   
-    
-    private function validateToken(string $token):JWT
+    public static function base64url_decode(string $data): string
+    {
+        return base64_decode(str_pad(strtr($data, '-_', '+/'), strlen($data) % 4, '=', STR_PAD_RIGHT));
+    }
+
+    private function validateToken(string $token): JWT
     {
 
         if (empty($token)) {
@@ -31,14 +33,14 @@ class JWT
 
         $keyPublicPath = "storage/public.key";
 
-        $part = explode(".",$token);
+        $part = explode(".", $token);
 
         $header = $part[0];
         $tokenPayload = $part[1];
         $signature = $part[2];
-        
+
         $payload = JWT::base64url_decode($tokenPayload);
-        $payload = json_decode($payload,true);
+        $payload = json_decode($payload, true);
 
         // check if is expired here
         $dt = new DateTimeImmutable;
@@ -54,8 +56,8 @@ class JWT
         $encodedData = $signature;
 
         // Open public path and return this in string format
-        $fp = fopen($keyPublicPath,"r");
-        $chavePublicaString = fread($fp,8192);
+        $fp = fopen($keyPublicPath, "r");
+        $chavePublicaString = fread($fp, 8192);
         fclose($fp);
 
         // Open public key string and return 'resourse'
@@ -68,15 +70,15 @@ class JWT
 
         $partialDecodedData = '';
         $decodedData = '';
-        $split2 = explode('.',$rawEncodedData);
-        foreach($split2 as $part2){
+        $split2 = explode('.', $rawEncodedData);
+        foreach ($split2 as $part2) {
             $part2 = JWT::base64url_decode($part2);
-            
+
             openssl_public_decrypt($part2, $partialDecodedData, $resPublicKey);
             $decodedData .= $partialDecodedData;
         }
 
-        if($header.".".$tokenPayload === $decodedData){
+        if ($header . "." . $tokenPayload === $decodedData) {
             // valid token
             $this->isValid = true;
             $this->payload = $payload;
@@ -87,21 +89,20 @@ class JWT
     }
 
 
-    
+
 
     /**
      * get access token from header
-    * */
+     * */
     static public function getBearerToken(): string
     {
 
-        function getAuthorizationHeader(): string 
+        function getAuthorizationHeader(): string
         {
             $headers = '';
             if (isset($_SERVER['Authorization'])) {
                 $headers = trim($_SERVER["Authorization"]);
-            }
-            else if (isset($_SERVER['HTTP_AUTHORIZATION'])) { //Nginx or fast CGI
+            } else if (isset($_SERVER['HTTP_AUTHORIZATION'])) { //Nginx or fast CGI
                 $headers = trim($_SERVER["HTTP_AUTHORIZATION"]);
             } elseif (function_exists('apache_request_headers')) {
                 $requestHeaders = apache_request_headers();
@@ -125,5 +126,4 @@ class JWT
         }
         return '';
     }
-
 }
