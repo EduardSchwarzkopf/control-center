3e7cd0b49a5a2397aafb896f8995ad871d4ca2bc
Eduard Schwarzkopf <mail.eduard.schwarzkopf@gmail.com>
2022-03-16 05:37:18 +0100

    validation rules added

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -9,5 +9,13 @@ class Setting extends Model
 {
     use HasFactory;
 
+    public const VALIDATION_RULES = [
+        'name' => ['required', 'string'],
+        'label' => ['required', 'string'],
+        'value' => ['required', 'string'],
+        'type' => ['required', 'string'],
+
+    ];
+
     protected $fillable = ['name', 'label', 'value'];
 }
