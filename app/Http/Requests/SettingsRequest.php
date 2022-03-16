053f530f43e0d1524f42487a66bb39c3b25b5368
Eduard Schwarzkopf <mail.eduard.schwarzkopf@gmail.com>
2022-03-16 06:18:16 +0100

    update and create logic update

--- app/Http/Requests/SettingsRequest.php
+++ app/Http/Requests/SettingsRequest.php
@@ -27,15 +27,52 @@ class SettingsRequest extends FormRequest
     public function rules()
     {
         $request = $this->request;
+        $method = $this->getMethod();
+        $rules = Setting::VALIDATION_RULES;
+
+        if ($method == 'POST') {
+            $rules = $this->handleCreateSetting($request, $rules);
+        }
+
+        if ($method == 'PUT') {
+            $rules = $this->handleUpdateSetting($request, $rules);
+        }
+
+        return $rules;
+    }
+
+    private function handleCreateSetting($request, array $rules): array
+    {
+
         $type = $request->get('type');
 
         if ($type == null) {
             $type = 'string';
         }
 
-        $rules = Setting::VALIDATION_RULES;
-
         $rules['value'] = ['required', $type];
+
+        return $rules;
+    }
+
+    private function handleUpdateSetting($request, array $rules): array
+    {
+        $settingId = $this->route('setting.id');
+        $setting = Setting::find($settingId);
+
+        $type = $request->get('type');
+        $value = $request->get('value');
+
+        if ($type) {
+            $request->set('value', $setting->value);
+            $rules['value'] = ['required', $type];
+        }
+
+        if ($value) {
+            $settingType = $setting->type;
+            $request->set('type', $settingType);
+            $rules['value'] = ['required', $settingType];
+        }
 
         return $rules;
     }
