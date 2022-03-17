85e29e67e03b2b31766c3643bb301114b992d378
Eduard Schwarzkopf <mail.eduard.schwarzkopf@gmail.com>
2022-03-17 06:34:01 +0100

    seeder updated

--- database/seeders/SettingsSeeder.php
+++ database/seeders/SettingsSeeder.php
@@ -13,24 +13,29 @@ class SettingsSeeder extends Seeder
      * @return void
      */
 
-    public function run() {
+    public function run()
+    {
         $settingsList = [
             [
                 'name' => 'client_api_url',
                 'label' => 'Client API URL',
                 'value' => '/wrkbeat-client/api.php',
+                'type' => 'string',
             ], [
                 'name' => 'monitor_history',
                 'label' => 'Monitor history',
                 'value' => '180',
+                'type' => 'integer',
             ], [
                 'name' => 'backup_max_allowed_size',
                 'label' => 'Backup maximum file size ',
                 'value' => '50',
+                'type' => 'integer',
             ], [
                 'name' => 'remote_backups_location',
                 'label' => 'Remote backup storage folder',
                 'value' => './client-backups',
+                'type' => 'string',
             ]
         ];
 
