cfe7fedd54f926353b6ca0af63460adf5b909627
Eduard Schwarzkopf <mail.eduard.schwarzkopf@gmail.com>
2022-03-19 11:46:03 +0100

    added barebone logic

--- wrkbeat-client/api.php
+++ wrkbeat-client/api.php
@@ -1,16 +1,47 @@
 <?php
-define('__ROOT__', dirname(dirname(__FILE__)));
-require_once('./JWT.php');
+define('__ROOT__', dirname(__FILE__));
+$root = __ROOT__;
+require_once(__ROOT__ . '/JWT.php');
 
 $token = JWT::getBearerToken();
+
 if ($token == null) {
-    echo json_encode(['message' => 'No token provided']);
-} else {
-
-    $jwt = new JWT($token);
-    $isValid = $jwt->isValid;
-    $infoText = "token is valid: $isValid";
-
-
-    echo json_encode(["is_valid" => $isValid, 'postData' => $_POST]);
+    // No Token provided
+    $requiredTokenResponseCode = 404;
+    http_response_code($requiredTokenResponseCode);
+    echo json_encode(['message' => 'Token required']);
+    return;
 }
+
+$jwt = new JWT($token);
+$isValid = $jwt->isValid;
+
+if ($isValid == false) {
+    $invalidTokenResponseCode = 401;
+    http_response_code($invalidTokenResponseCode);
+    echo json_encode(['message' => 'Invalid token']);
+    return;
+}
+
+// All ok, lets make some checks
+
+$postData = $_POST;
+
+// Start all Process
+// SQLConnectionCheck
+// BackupProcess
+// BackupCheck
+// CheckSendingMail
+// InodesCheck
+// DiskusageCheck
+
+$checkResults = [];
+if (in_array(false, $checkResults)) {
+    $responseCode = 503; // Error
+} else {
+    $responseCode = 200; // Success
+}
+
+http_response_code($responseCode);
+
+echo json_encode(["is_valid" => $isValid, 'postData' => $postData]);
