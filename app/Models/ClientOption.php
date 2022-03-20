2e35763e2f02056584881ef21933649089413d93
Eduard Schwarzkopf <mail.eduard.schwarzkopf@gmail.com>
2022-03-20 16:40:50 +0100

    attribute renaming

--- app/Models/ClientOption.php
+++ app/Models/ClientOption.php
@@ -30,30 +30,45 @@ class ClientOption extends Model
         'backup_database_amount',
         'backup_files_max_age',
         'backup_files_amount',
-        'diskspace_warn_level',
-        'inodes_warn_level',
+        'diskspace_threshold',
+        'indoes_threshold',
         'email_receiver'
     ];
-    
-     protected $attributes = [
-        'is_active' => true,
+
+    protected $attributes = [
         'check_interval' => 900,
         'test_retries' => 0,
         'retry_interval' => 900,
         'client_environment_id' => 1,
 
-        'inodes_enabled' => false,
-        'backup_files_enabled' => false,
-        'backup_database_enabled' => false,
-        'diskspace_enabled' => false,
-        'email_enabled' => false,
-        'backup_notification_enabled' => false,
-        'backup_remote_enabled' => false
     ];
 
     protected $hidden = [
         'client_environment_id',
     ];
+
+    public function getClientPayload(): array
+    {
+
+        $requestData = [
+            'inodes_enabled' => false,
+            'backup_files_enabled' => false,
+            'backup_database_enabled' => false,
+            'diskspace_enabled' => false,
+            'email_enabled' => false,
+            'backup_notification_enabled' => false,
+            'backup_remote_enabled' => false,
+        ];
+
+        foreach ($requestData as $key => $value) {
+
+            $requestData[$key] = boolval($this[$key]);
+        }
+
+        $requestData['environment'] = $this['clientEnvironment']->name;
+
+        return $requestData;
+    }
 
     protected $with = ['clientEnvironment'];
 
@@ -61,7 +76,7 @@ class ClientOption extends Model
     {
         return $this->belongsTo(Client::class);
     }
-    
+
     public function clientEnvironment()
     {
         return $this->belongsTo(ClientEnvironment::class);
