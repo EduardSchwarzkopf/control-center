f0403be6289f013b306b3c397289aeb66148984f
48969167 <mail.eduard.schwarzkopf@gmail.com>
2022-03-21 07:04:54 +0100

    use generic config loader

--- wrkbeat-client/Platforms/WordpressPlatform.php
+++ wrkbeat-client/Platforms/WordpressPlatform.php
@@ -6,11 +6,11 @@ class WordpressPlatform extends Platform
 
     function __construct()
     {
-
-        include_once($rootPath . '/wp-config.php');
+        $configFilePath = '/wp-config.php';
+        parent::__construct($configFilePath);
 
         $this->_host = DB_HOST;
-        $this->_dbname = DB_NAME;
+        $this->_database = DB_NAME;
         $this->_username = DB_USER;
         $this->_password = DB_PASSWORD;
     }
