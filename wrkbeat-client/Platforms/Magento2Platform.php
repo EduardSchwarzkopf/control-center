88576e22d38761101c7aa4e2f7cf5040573144aa
48969167 <mail.eduard.schwarzkopf@gmail.com>
2022-03-21 07:04:46 +0100

    use generic config loader

--- wrkbeat-client/Platforms/Magento2Platform.php
+++ wrkbeat-client/Platforms/Magento2Platform.php
@@ -1,14 +1,16 @@
 <?php
 
 
-class Magento2 extends Platform
+class Magento2Platform extends Platform
 {
 
     function __construct()
     {
-        $rootPath = $this->GetPlatformRootPath();
-        $env = include_once($rootPath . '/app/etc/env.php');
-        $credentials = $env["db"]['connection']['default'];
+
+        $configPath = '/app/etc/env.php';
+        parent::__construct($configPath);
+
+        $credentials = $this->_platformConfig["db"]['connection']['default'];
         $this->_host = $credentials["host"];
         $this->_database = $credentials["dbname"];
         $this->_username = $credentials["username"];
