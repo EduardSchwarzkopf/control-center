afaf05f4965005867e90d79e1f1d06403f6ebf84
48969167 <mail.eduard.schwarzkopf@gmail.com>
2022-03-22 07:13:50 +0100

    updated checks

--- wrkbeat-client/Monitor.php
+++ wrkbeat-client/Monitor.php
@@ -2,17 +2,19 @@
 
 class Monitor
 {
-    private array $_responseList = [];
-    private array $_errorList = [];
+    private array $_result = [];
+    private array $_info = [];
+    private array $_error = [];
 
-    public function StartChecks()
+    public function StartChecks(array $checkList): void
     {
-
-        $checkList = $_POST;
 
         $checkList = [
             'diskusage' => 80,
             'inodes' => 80,
+            'backup_database' => 24,
+            'backup_files' => 24,
+            'email' => 'my@email.com',
             'wordpress' => true,
         ];
 
@@ -22,26 +24,44 @@ class Monitor
             switch ($checkItem) {
                 case 'diskusage':
                     $result = CheckDiskUsage::Run($value);
-                    $this->_diskusage = $result;
                     break;
 
                 case 'inodes':
                     $result = CheckInodes::Run($value);
-                    $this->_inodes = $result;
+                    break;
+
+                case 'backup_database':
+                case 'backup_files':
+
+                    $stringList = explode('_', $checkItem);
+                    $checkClassname = 'Check';
+
+                    foreach ($stringList as $string) {
+                        $checkClassname .= ucfirst($string);
+                    }
+
+                    try {
+                        $backupCheck = new $checkClassname;
+                    } catch (Exception $e) {
+                        $message = $e->getMessage();
+                        $line = $e->getLine();
+                        $file = $e->getFile();
+
+                        $this->_error[$checkClassname] = "BACKUP_CHECK ERROR: $message in $file on line $line";
+                        break;
+                    }
+
+                    $result = $backupCheck::Run($value);
+
                     break;
 
                 case 'email':
-                    $result = CheckInodes::Run($value);
-                    $this->_email = $result;
+                    $result = CheckEmail::Run($value);
                     break;
 
                 case 'wordpress':
                 case 'magento1':
                 case 'magento2':
-
-                    if ($value == false) {
-                        break;
-                    }
 
                     $platformName = ucfirst($checkItem) . 'Platform';
 
@@ -52,7 +72,7 @@ class Monitor
                         $line = $e->getLine();
                         $file = $e->getFile();
 
-                        $this->_errorList[$platformName] = "PLATFORM ERROR: $message in $file on line $line";
+                        $this->_error[$platformName] = "PLATFORM ERROR: $message in $file on line $line";
                         break;
                     }
 
@@ -60,13 +80,12 @@ class Monitor
                     $result = $platformCheck::Run($platform);
 
                     $serverInfo = $platform->db_server_info;
-                    $this->_responseList['db_server_info'] = $serverInfo;
+                    $this->_info['db_server_info'] = $serverInfo;
 
-                    $a = 1;
                     break;
             }
 
-            $a = 1;
+            $this->_result[$checkItem] = $result;
         }
     }
 
