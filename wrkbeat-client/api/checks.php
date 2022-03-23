836be3aad4da9c74958c29506f21ee8287ecaae5
48969167 <mail.eduard.schwarzkopf@gmail.com>
2022-03-23 07:27:30 +0100

    refactored for prod

--- wrkbeat-client/api/checks.php
+++ wrkbeat-client/api/checks.php
@@ -1,47 +1,32 @@
 <?php
 
-define('__ROOT__', dirname(dirname(__FILE__)));
+define('__ROOT__', dirname(__FILE__, 2));
 include(__ROOT__ . '/Autoloader.php');
 
-// $token = JWT::getBearerToken();
+$token = JWT::getBearerToken();
 
-// if ($token == null) {
-//     // No Token provided
-//     $requiredTokenResponseCode = 404;
-//     http_response_code($requiredTokenResponseCode);
-//     echo json_encode(['message' => 'Token required']);
-//     return;
-// }
+if ($token == null) {
+    $requiredTokenResponseCode = 404;
+    http_response_code($requiredTokenResponseCode);
+    echo json_encode(['message' => 'Token required']);
+    return;
+}
 
-// $jwt = new JWT($token);
-// $isValid = $jwt->isValid;
+$jwt = new JWT($token);
+$isValid = $jwt->isValid;
 
-// if ($isValid == false) {
-//     $invalidTokenResponseCode = 401;
-//     http_response_code($invalidTokenResponseCode);
-//     echo json_encode(['message' => 'Invalid token']);
-//     return;
-// }
-
-// All ok, lets make some checks
+if ($isValid == false) {
+    $invalidTokenResponseCode = 401;
+    http_response_code($invalidTokenResponseCode);
+    echo json_encode(['message' => 'Invalid token']);
+    return;
+}
 
 $postData = $_POST;
-
-// SQLConnectionCheck
-// BackupCheck
-// CheckSendingMail
 
 $monitor = new Monitor;
 $monitor->StartChecks($_POST);
 
+$response = $monitor->JSONResponse();
 
-$checkResults = [];
-if (in_array(false, $checkResults)) {
-    $responseCode = 503; // Error
-} else {
-    $responseCode = 200; // Success
-}
-
-http_response_code($responseCode);
-
-echo json_encode(["is_valid" => $isValid, 'postData' => $postData]);
+echo $response;
