0e936e57f574b0c901d5f4b9af51f55304c9a9d7
Eduard Schwarzkopf <mail.eduard.schwarzkopf@gmail.com>
2022-03-24 18:49:40 +0100

    basicly renewed

--- wrkbeat-client/Platforms/Platform.php
+++ wrkbeat-client/Platforms/Platform.php
@@ -1,35 +1,21 @@
 <?php
 
-abstract class Platform implements ResponseInterface
+abstract class Platform
 {
 
-    protected string $_host = '';
-    protected string $_database = '';
-    protected string $_username = '';
-    protected string $_password = '';
-    protected string $_platformRoot = '';
-    protected $_platformConfig = '';
-
-    protected string $_db_server_info = '';
-    protected string $_db_dump_path = '';
-    protected int $_db_file_size = 0;
-    protected string $_db_human_file_size = '';
-
-    protected string $_backup_dump_path = '';
-    protected int $_backup_file_size = 0;
-    protected string $_backup_human_file_size = '';
-
-    protected array $hideFields = [
-        'hideFields', '_host', '_database', '_username', '_password', '_platformRoot', '_platformConfig'
-    ];
-
+    protected string $host = '';
+    protected string $database = '';
+    protected string $username = '';
+    protected string $password = '';
+    protected string $platformRoot = '';
+    protected $platformConfig = '';
 
     function __construct($configFilePath)
     {
-        $this->_platformRoot = dirname(__DIR__, 2);
+        $this->platformRoot = dirname(__DIR__, 2);
 
-        $configPath = $this->_platformRoot . $configFilePath;
-        $this->_platformConfig = $this->LoadPlatformConfigFile($configPath);
+        $configPath = $this->platformRoot . $configFilePath;
+        $this->platformConfig = $this->LoadPlatformConfigFile($configPath);
     }
 
     private function LoadPlatformConfigFile(string $configFilePath)
@@ -37,7 +23,7 @@ abstract class Platform implements ResponseInterface
         $platformConfig = include_once($configFilePath);
 
         if ($platformConfig == false) {
-            $platformConfig = simplexml_load_file($this->_platformRoot . '/app/etc/local.xml');
+            $platformConfig = simplexml_load_file($this->platformRoot . '/app/etc/local.xml');
 
             if ($platformConfig == false) {
                 throw new Exception($configFilePath . ' not found');
@@ -47,58 +33,31 @@ abstract class Platform implements ResponseInterface
         return $platformConfig;
     }
 
-    public function GetHiddenFields()
+    private function GetResponseList(): array
     {
-        return $this->GetHiddenFields();
+        return [
+            'result'=>false,
+            'bytes'=>0,
+            'path'=>null,
+            'size'=>0,
+        ];
     }
 
-    public function GetBackupDumpPath(): string
-    {
-        return $this->_backup_dump_path;
-    }
-
-    public function GetBackupFileSize(): int
-    {
-        return $this->_backup_file_size;
-    }
-
-    public function GetBackupHumanFileSize(): string
-    {
-        return $this->_backup_human_file_size;
-    }
-
-    public function GetSQLDumpPath(): string
-    {
-        return $this->_db_dump_path;
-    }
-
-    public function GetDatabaseFileSize(): int
-    {
-        return $this->_db_file_size;
-    }
-
-    public function GetDatabaseHumanFileSize(): string
-    {
-        return $this->_db_human_file_size;
-    }
-
-    public function GetDatabaseInfo(): string
-    {
-        return $this->_db_server_info;
-    }
-
-    public function CreateSQLDump(): bool
+    public function CreateSQLDump(): array
     {
         $sqlCheck = $this->CheckDatabaseConnection();
 
+        $responseList = $this->GetResponseList();
+        $responseList['db_version'] = $this->db_server_info;
+
         if ($sqlCheck == false) {
-            return false;
+            return $responseList;
         }
 
-        $host = $this->_host;
-        $database = $this->_database;
-        $username = $this->_username;
-        $password = $this->_password;
+        $host = $this->host;
+        $database = $this->database;
+        $username = $this->username;
+        $password = $this->password;
 
         $randomString = Utils::RandomString();
 
@@ -108,16 +67,16 @@ abstract class Platform implements ResponseInterface
         $cmd = "mysqldump --user=$username  --password=$password  --host=$host  --routines --skip-triggers --lock-tables=false --default-character-set=utf8  $database --single-transaction=TRUE | gzip > $dumpFilePath";
         exec($cmd);
 
-        $result = file_exists($dumpFilePath);
+        if (file_exists($dumpFilePath)) {
 
-        if ($result) {
-
-            $this->_db_dump_path = str_replace($this->_platformRoot, '', $dumpFilePath);
-            $this->_db_file_size = filesize($dumpFilePath);
-            $this->_db_human_file_size = FileUtils::HumanFileSize($this->_db_file_size);
+            $bytes = filesize($dumpFilePath);
+            $responseList['result'] = true;
+            $responseList['path'] = str_replace($this->platformRoot, '', $dumpFilePath);
+            $responseList['bytes'] = $bytes;
+            $responseList['size'] = FileUtils::HumanFileSize($bytes);
         }
 
-        return $result;
+        return $responseList;
     }
 
     public function CheckDatabaseConnection(): bool
@@ -125,13 +84,13 @@ abstract class Platform implements ResponseInterface
         try {
 
             $result = true;
-            $conn = new mysqli($this->_host, $this->_username, $this->_password, $this->_database);
+            $conn = new mysqli($this->host, $this->username, $this->password, $this->database);
 
             if ($conn->connect_error || $conn->error) {
                 $result = false;
             }
 
-            $this->_db_server_info = $conn->server_info;
+            $this->db_server_info = $conn->server_info;
 
             $conn->close();
             return $result;
@@ -140,36 +99,48 @@ abstract class Platform implements ResponseInterface
         }
     }
 
-    public function CreateFilesBackup(?array $exludePatternList): bool
+    //
+    // Doku: Reponse ggf. modularer gestalten für die Zukunft, ist aber aufgrund von Projektbudget nicht vorgehsen
+    //
+    public function CreateFilesBackup(?array $exludePatternList): array
     {
-        $result = true;
-
+        $responseList = $this->GetResponseList();
 
         $exlude = '';
-        $platformPath = $this->_platformRoot;
-        foreach ($exludePatternList as $excludePattern) {
-            $exlude .= "--exclude=$platformPath/$excludePattern ";
-        }
+        $rootPath = dirname(__DIR__, 3);
+        $platformPath = $this->GetRelativeFilePath($this->platformRoot);
 
         $now = date('Y-m-d_H-i-s');
         $randomString = Utils::RandomString();
 
         $file = $now . '_files_backup_' . $randomString . '.tgz';
 
-        $clientPath = dirname(__DIR__);
-        $backupPath = $clientPath . '/backups/' . $file;
-        $cmd = "tar zcv --exclude=$clientPath $exlude -f $backupPath $platformPath ";
-        $exec = exec($cmd);
+        $absoluteClientPath = dirname(__DIR__);
 
-        $result = file_exists($backupPath);
-        if ($result) {
+        $clientPath = $this->GetRelativeFilePath($absoluteClientPath);
+        $backupPath = $absoluteClientPath . '/backups/' . $file;
 
-            $this->_backup_dump_path = str_replace($this->_platformRoot, '', $backupPath);
-            $this->_backup_file_size = filesize($backupPath);
-            $this->_backup_human_file_size = FileUtils::HumanFileSize($this->_backup_file_size);
+        foreach ($exludePatternList as $excludePattern) {
+            $exlude .= "--exclude=$excludePattern ";
         }
 
+        $cmd = "tar -cvz --exclude=$clientPath $exlude -C $rootPath -f $backupPath $platformPath";
+        exec($cmd);
 
-        return $result;
+        if (file_exists($backupPath)) {
+
+            $bytes = filesize($backupPath);
+            $responseList['result'] = true;
+            $responseList['path'] = str_replace($this->platformRoot, '', $backupPath);
+            $responseList['bytes'] = $bytes;
+            $responseList['size'] = FileUtils::HumanFileSize($bytes);
+        }
+
+        return $responseList;
+    }
+
+    protected function GetRelativeFilePath($filePath) {
+        $rootPath = dirname(__DIR__, 3) . '/';
+        return str_replace($rootPath, '', $filePath);
     }
 }
