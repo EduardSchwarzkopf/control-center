369fb814a4f9a78f898bc0cd6188bd014a93bb4d
Eduard Schwarzkopf <mail.eduard.schwarzkopf@gmail.com>
2022-03-25 10:31:35 +0100

    removed obselete code

--- wrkbeat-client/api/checks.php
+++ wrkbeat-client/api/checks.php
@@ -1,30 +1,9 @@
 <?php
 
-define('__ROOT__', dirname(__DIR__));
-include(__ROOT__ . '/Autoloader.php');
-
-// $token = JWT::getBearerToken();
-
-// if ($token == null) {
-//     $requiredTokenResponseCode = 404;
-//     http_response_code($requiredTokenResponseCode);
-//     echo json_encode(['message' => 'Token required']);
-//     return;
-// }
-
-// $jwt = new JWT($token);
-// $isValid = $jwt->isValid;
-
-// if ($isValid == false) {
-//     $invalidTokenResponseCode = 401;
-//     http_response_code($invalidTokenResponseCode);
-//     echo json_encode(['message' => 'Invalid token']);
-//     return;
-// }
-
-$postData = $_POST;
 
 $monitor = new Monitor;
 $monitor->StartChecks($_POST);
 
-echo $res;
+$response = $monitor->JSONResponse();
+
+echo $response;
