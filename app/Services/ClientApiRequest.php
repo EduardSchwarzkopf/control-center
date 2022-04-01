2a311093688f5ff849db6e31c9276e5ace8189c5
Eduard Schwarzkopf <mail.eduard.schwarzkopf@gmail.com>
2022-04-01 18:28:52 +0200

    ClientApiRequest init

---
+++ app/Services/ClientApiRequest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Services;
+
+use App\Services\JWTService;
+use GuzzleHttp\Client as GuzzleHttpClient;
+use GuzzleHttp\Exception\GuzzleException;
+use Illuminate\Support\Facades\Log;
+use Psr\Http\Message\ResponseInterface;
+
+class ClientApiRequest
+{
+
+    private int $expiresAfterSeconds = 1;
+    private ?JWTService $jwtService = null;
+
+    public function __construct()
+    {
+        $this->jwtService = new JWTService();
+        $this->jwtService->expiresAfter($this->expiresAfterSeconds);
+    }
+
+    private function CreateAccessToken(): string
+    {
+        return $this->jwtService->createToken();
+    }
+
+    public function get(string $url, array $parameter): ?ResponseInterface
+    {
+        return $this->request('GET', $url, ['query' => $parameter]);
+    }
+
+    public function post(string $url, array $form_params): ?ResponseInterface
+    {
+        return  $this->request('POST', $url, ['form_params' => $form_params]);
+    }
+
+    public function delete($url): ?ResponseInterface
+    {
+        return $this->request('DELETE', $url);
+    }
+
+    private function request(string $method, string $url, array $payload = []): ?ResponseInterface
+    {
+        $token = $this->CreateAccessToken();
+        $request = new GuzzleHttpClient();
+        $res = null;
+
+        $options = ['headers' => ['Authorization' => 'Bearer ' . $token]] + $payload;
+
+        try {
+            $res = $request->request($method, $url, $options);
+        } catch (GuzzleException $e) {
+            $message = $e->getMessage();
+            $this->logging($message);
+        }
+
+        return $res;
+    }
+
+    private function logging($message): void
+    {
+        $now = date("Y-m-d H:i:s");
+        Log::warning($now . ' - CLIENT_API: ' . $message);
+    }
+}
