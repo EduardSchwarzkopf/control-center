f9285ee710ccedc9e414512147aa3961e497c5de
Eduard Schwarzkopf <mail.eduard.schwarzkopf@gmail.com>
2022-04-01 18:29:16 +0200

    removed obselete fields

--- database/migrations/2022_03_05_044631_create_client_options_table.php
+++ database/migrations/2022_03_05_044631_create_client_options_table.php
@@ -22,28 +22,17 @@ return new class extends Migration
             $table->foreign('client_id')->references('id')->on('clients')->onDelete('cascade');
 
             $table->integer('check_interval');
-            $table->integer('test_retries');
-            $table->integer('retry_interval');
 
-            $table->boolean('inodes_enabled');
-            $table->boolean('backup_files_enabled');
             $table->boolean('backup_database_enabled');
-            $table->boolean('diskspace_enabled');
-            $table->boolean('email_enabled');
-            $table->boolean('backup_notification_enabled');
-            $table->boolean('backup_remote_enabled');
-
-            $table->string('backup_notification_receiver')->nullable();
-            $table->string('backup_custom_folder')->nullable();
-            $table->integer('backup_remote_amount')->nullable();
             $table->integer('backup_database_max_age')->nullable();
             $table->integer('backup_database_amount')->nullable();
+
+            $table->boolean('backup_files_enabled');
             $table->integer('backup_files_max_age')->nullable();
             $table->integer('backup_files_amount')->nullable();
 
             $table->float('diskspace_threshold')->nullable();
-            $table->float('indoes_threshold')->nullable();
-            $table->string('email_receiver')->nullable();
+            $table->float('inodes_threshold')->nullable();
         });
     }
 
