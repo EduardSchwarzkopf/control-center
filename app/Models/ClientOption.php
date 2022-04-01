cd814b4be544e942e2bab2b681d7c9ba05153acb
Eduard Schwarzkopf <mail.eduard.schwarzkopf@gmail.com>
2022-04-01 18:26:32 +0200

    removed obselete fields

--- app/Models/ClientOption.php
+++ app/Models/ClientOption.php
@@ -12,39 +12,20 @@ class ClientOption extends Model
     protected $fillable = [
         'client_id',
         'check_interval',
-        'test_retries',
-        'retry_interval',
-        'inodes_enabled',
-        'diskspace_enabled',
-        'email_enabled',
-        'backup_notification_enabled',
-        'backup_notification_receiver',
-        'backup_files_enabled',
         'backup_database_enabled',
-        'backup_custom_folder',
-        'backup_remote_enabled',
-        'remote_backup_amount',
         'backup_database_max_age',
         'backup_database_amount',
+        'backup_files_enabled',
         'backup_files_max_age',
         'backup_files_amount',
         'diskspace_threshold',
-        'indoes_threshold',
-        'email_receiver'
+        'inodes_threshold',
     ];
 
     protected $attributes = [
         'check_interval' => 900,
-        'test_retries' => 0,
-        'retry_interval' => 900,
-
-        'inodes_enabled' => false,
         'backup_files_enabled' => false,
         'backup_database_enabled' => false,
-        'diskspace_enabled' => false,
-        'email_enabled' => false,
-        'backup_notification_enabled' => false,
-        'backup_remote_enabled' => false
     ];
 
 
@@ -52,13 +33,8 @@ class ClientOption extends Model
     {
 
         $requestData = [
-            'inodes_enabled' => false,
             'backup_files_enabled' => false,
             'backup_database_enabled' => false,
-            'diskspace_enabled' => false,
-            'email_enabled' => false,
-            'backup_notification_enabled' => false,
-            'backup_remote_enabled' => false,
         ];
 
         foreach ($requestData as $key => $value) {
