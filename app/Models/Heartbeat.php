58c580cf6a251924369ca6c6426d9d291931681f
Eduard Schwarzkopf <mail.eduard.schwarzkopf@gmail.com>
2022-04-01 18:27:40 +0200

    heartbeat int

---
+++ app/Models/Heartbeat.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Heartbeat extends Model
+{
+    use HasFactory;
+    public $timestamps = false;
+
+    protected $fillable = ['client_id', 'type', 'status', 'message', 'value', 'expected_value' 'created_at'];
+
+    public function client()
+    {
+        return $this->belongsTo(Client::class);
+    }
+}
