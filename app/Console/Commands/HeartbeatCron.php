2d35a6ae95d72a7addb2ec8c363c236c2dda811c
48969167 <mail.eduard.schwarzkopf@gmail.com>
2022-04-03 09:53:40 +0200

    heartbeat logic added

--- app/Console/Commands/HeartbeatCron.php
+++ app/Console/Commands/HeartbeatCron.php
@@ -3,12 +3,13 @@
 namespace App\Console\Commands;
 
 use App\Models\Client;
+use App\Models\ClientOption;
 use App\Models\Heartbeat;
 use App\Services\ClientApiRequest;
 use GuzzleHttp\Client as GuzzleHttpClient;
 use GuzzleHttp\Exception\GuzzleException;
 use Illuminate\Console\Command;
-use stdClass;
+
 
 class HeartbeatCron extends Command
 {
@@ -44,12 +45,13 @@ class HeartbeatCron extends Command
 
     }
 
-    private function CreateHeartbeat(string $type, bool $status, string $message): void
+    private function CreateHeartbeat(string $type, bool $status, string $message, $value = null): void
     {
         Heartbeat::create([
             'client_id' => $this->clientId,
             'type' => $type,
             'status' => $status,
+            'value' => $value,
             'message' => $message,
         ]);
     }
@@ -70,14 +72,95 @@ class HeartbeatCron extends Command
         return $statusCode;
     }
 
-    private function CheckDiskusage(stdClass $jsonResponse, $threshold): void
+    private function CheckSystem(ClientOption $clientOptions, ClientApiRequest $clientRequest, $url): void
     {
+
+        $systemQueryList = [];
+        $diskUsageThreshold = $clientOptions->diskspace_threshold;
+        if (is_numeric($diskUsageThreshold) && $diskUsageThreshold > 0) {
+            $systemQueryList['diskusage'] = $diskUsageThreshold;
+        }
+
+        $inodesThreshold = $clientOptions->inodes_threshold;
+        if (is_numeric($inodesThreshold) && $inodesThreshold > 0) {
+            $systemQueryList['inodes'] = $inodesThreshold;
+        }
+
+        $responseList = $this->GetApiResponse($clientRequest, $url, $systemQueryList);
+
+        if ($responseList == null) {
+            $this->TriggerWarning();
+            return;
+        }
+
+
+        foreach ($systemQueryList as $checkItem => $threshold) {
+
+            $responseItem = $responseList['data'][$checkItem];
+            $checkStatus = $responseItem < $threshold;
+            $message = $checkItem . ' is now at ' . $responseItem . '%';
+
+            if ($checkStatus == false) {
+                $this->TriggerWarning();
+            }
+
+            $this->CreateHeartbeat($checkItem, $checkStatus, $responseItem, $message);
+        }
+    }
+
+    private function CheckBackups(ClientOption $clientOptions, ClientApiRequest $clientRequest, $url): void
+    {
+
+        $backupQueryParameterList = [];
+        $databaseMaxHours = $clientOptions->backup_database_max_age;
+        if (is_numeric($databaseMaxHours) && $databaseMaxHours > 0) {
+            $backupQueryParameterList['file'] = $databaseMaxHours;
+        }
+
+        $fileMaxHours = $clientOptions->backup_database_max_age;
+        if (is_numeric($fileMaxHours) && $fileMaxHours > 0) {
+            $backupQueryParameterList['database'] = $fileMaxHours;
+        }
+
+        $responseList = $this->GetApiResponse($clientRequest, $url, $backupQueryParameterList);
+
+        foreach ($backupQueryParameterList as $checkItem => $threshold) {
+            $responseItem = $responseList['data'][$checkItem]['hours'];
+            $checkStatus = $responseItem < $threshold;
+
+            if ($checkStatus == false) {
+                $this->TriggerWarning();
+            }
+
+            $this->CreateHeartbeat($checkItem, $checkStatus, $responseItem, '');
+        }
+    }
+
+    private function GetApiResponse(ClientApiRequest $clientRequest, string $url, array $queryParameterList): array
+    {
+        $clientResponse = $clientRequest->get($url, $queryParameterList);
+
+
+        if ($clientResponse == null) {
+            $this->TriggerWarning();
+            return [];
+        }
+
+        $responseList = json_decode($clientResponse->getBody(), true);
+
+
+        if ($responseList == null || count($responseList) == 0) {
+            $this->TriggerWarning();
+            return [];
+        }
+
+        return $responseList;
     }
 
     /**
      * Execute the console command.
      *
-     * debug with: php -dxdebug.mode=debug -dxdebug.start_with_request=yes artisan client:cron
+     * debug with: php -dxdebug.mode=debug -dxdebug.start_with_request=yes artisan heartbeat:cron
      */
     public function handle()
     {
@@ -91,15 +174,22 @@ class HeartbeatCron extends Command
             $envName = $clientEnvironment->name;
             $clientOptions = $client->options;
 
-            $now = date("Y-m-d H:i:s");
-
-            // TODO: Check for check_interval
-
             $isActive = $client->is_active;
-
             if ($isActive == false) {
                 continue;
             }
+
+            // Time to check again?
+            $heartbeat = Heartbeat::where('client_id', "=", $client->id)->first();
+
+            if (false) {
+
+                $dbtimestamp = strtotime($heartbeat->created_at);
+                if (time() - $dbtimestamp < $clientOptions->check_interval) {
+                    continue;
+                }
+            }
+
 
             $apiUrl = config('app.client_api_url');
             $clientUrl = $client->url;
@@ -108,67 +198,15 @@ class HeartbeatCron extends Command
             $expectedStatusCode = $client->status_code ? $client->status_code : 200;
             $type = 'status_code';
             $checkStatusMessage = "Expected: $expectedStatusCode ; Expected: $checkStatusCode";
-            $this->CreateHeartbeat($type, $expectedStatusCode == $checkStatusCode, $checkStatusMessage);
+            $this->CreateHeartbeat($type, $expectedStatusCode == $checkStatusCode, $checkStatusCode, $checkStatusMessage);
 
             if ($checkStatusCode != $expectedStatusCode) {
                 $this->TriggerWarning();
             }
 
-
-            $queryParameters = [];
-            if ($envName != 'None') {
-                $queryParameters['platform'] = $envName;
-            }
-
-            if (is_numeric($clientOptions->backup_database_max_age)) {
-                // TODO: Add to db check
-            }
-
-            if (is_numeric($clientOptions->backup_files_max_age)) {
-                // TODO: Add to db check
-            }
-
-            $diskUsageThreshold = $clientOptions->diskspace_threshold;
-            if (is_numeric($diskUsageThreshold) && $diskUsageThreshold > 0) {
-                $queryParameters['diskusage'] = true;
-            }
-
-            $inodesThreshold = $clientOptions->inodes_threshold;
-            if (is_numeric($inodesThreshold) && $inodesThreshold > 0) {
-                $queryParameters['inodes'] = true;
-            }
-
-            $systemApi = $clientUrl . $apiUrl . '/system';
-            $response = $clientRequest->get($systemApi, $queryParameters);
-
-            $jsonResponse = json_decode($response->getBody(), true);
-
-            if ($jsonResponse == null) {
-                $this->TriggerWarning();
-                continue;
-            }
-
-            foreach ($queryParameters as $checkItem => $value) {
-
-                $checkStatus = null;
-                switch ($checkItem) {
-                    case 'diskusage':
-                    case 'inodes':
-                        $responseItem = $jsonResponse['data'][$checkItem];
-                        $checkStatus = $responseItem < 80;
-                        $message = $checkItem . ' is now at ' . $responseItem . '%';
-                        break;
-
-                    default:
-                        break;
-                }
-
-                if ($checkStatus == null) {
-                    continue;
-                }
-
-                $this->CreateHeartbeat($checkItem, $checkStatus, $message);
-            }
+            $baseUrl = $clientUrl . $apiUrl;
+            $this->CheckBackups($clientOptions, $clientRequest, $baseUrl . '/backups');
+            $this->CheckSystem($clientOptions, $clientRequest, $baseUrl . '/system');
         }
     }
 }
