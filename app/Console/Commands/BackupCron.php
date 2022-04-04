11debd761fc3ced19aa077e4bf2762c9e2e83421
Eduard Schwarzkopf <mail.eduard.schwarzkopf@gmail.com>
2022-04-04 20:54:51 +0200

    wip - added logic

--- app/Console/Commands/BackupCron.php
+++ app/Console/Commands/BackupCron.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Client;
 use App\Models\Heartbeat;
 
 class BackupCron extends ClientCron
@@ -36,8 +37,34 @@ class BackupCron extends ClientCron
         return 'backup_cron';
     }
 
-    protected function RunCron(): void
+    protected function GetClientList()
     {
+        return Client::where('is_active', "=", true)
+            ->where(function ($query) {
+                $query->orWhere('backup_files_enabled', '=', true)
+                    ->orWhere('backup_database_enabled', '=', true);
+            })
+            ->get();
+    }
+
+    private function ClientCreateBackup(string $type): void
+    {
+        // TODO: Add Logic here
+    }
+
+    private function ClientRotateBackups(string $type): void
+    {
+        // TODO: Add logic here
+    }
+
+    private function RotateBackups(string $type): void
+    {
+        // TODO: Add Logic
+    }
+
+    private function PullBackup(string $type): void
+    {
+        // TODO: Add Logic
     }
 
 
@@ -46,24 +73,39 @@ class BackupCron extends ClientCron
      *
      * debug with: php -dxdebug.mode=debug -dxdebug.start_with_request=yes artisan backup:cron
      */
-    public function handle()
+    protected function RunCron(): void
     {
 
         $client = $this->client;
+        $clientOptions = $this->getClientOptions;
         $heartbeat = Heartbeat::where([['client_id', "=", $client->id], ['type', '=', $this->heartbeatType]])->first();
+
 
         if ($heartbeat) {
 
             $dbtimestamp = strtotime($heartbeat->created_at);
-            if (time() - $dbtimestamp < $this->clientOptions->check_interval) {
+            if (time() - $dbtimestamp < $clientOptions->backup_interval * 60 * 60) // hours
+            {
                 return;
             }
         }
 
-        // TODO: Add to Client model: backup_database_interval, backup_file_interval
-        // TODO: Create Backup
-        // TODO: Pull Backup
-        // TODO: Rotate Backup
-        // TODO: Rotate Backup on Client
+        $backupTypes = ['files', 'database'];
+        foreach ($backupTypes as $type) {
+
+            $key = 'backup_' . $type;
+            $this->ClientCreateBackup($type);
+
+            $remoteAmount = $clientOptions[$key . 'amount_remote'];
+            if (is_numeric($remoteAmount)) {
+                $this->ClientRotateBackups($type, $remoteAmount);
+            }
+
+            $localAmount = $clientOptions[$key . 'amount_remote'];
+            if (is_numeric($localAmount)) {
+                $this->PullBackup($type);
+                $this->RotateBackups($type, $remoteAmount);
+            }
+        }
     }
 }
