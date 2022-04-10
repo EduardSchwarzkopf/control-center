7aab06ec4668d6fc6a9d47ec979915df3a1cd422
48969167 <mail.eduard.schwarzkopf@gmail.com>
2022-04-10 15:43:04 +0200

    refactored for layout functionality

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,66 +1,75 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
-import ClientsIndex from '../components/clients/ClientsIndex.vue'
-import ClientsForm from '../components/clients/ClientsForm.vue'
-import UsersIndex from '../components/users/UsersIndex.vue'
-import UpdateUsersForm from '../components/users/UpdateUsersForm.vue'
-import CreateUsersForm from '../components/users/CreateUsersForm.vue'
-import SettingsIndex from '../components/settings/SettingsIndex.vue'
-import UpdateSettingsForm from '../components/settings/CreateSettingsForm.vue'
-import CreateSettingsForm from '../components/settings/UpdateSettingsForm.vue'
+import ClientsIndex from "../components/clients/ClientsIndex.vue";
+import ClientsForm from "../components/clients/ClientsForm.vue";
+import UsersIndex from "../components/users/UsersIndex.vue";
+import UpdateUsersForm from "../components/users/UpdateUsersForm.vue";
+import CreateUsersForm from "../components/users/CreateUsersForm.vue";
+import SettingsIndex from "../components/settings/SettingsIndex.vue";
+import UpdateSettingsForm from "../components/settings/CreateSettingsForm.vue";
+import CreateSettingsForm from "../components/settings/UpdateSettingsForm.vue";
+
+import Layout from "../components/layouts/Layout.vue";
 
 const routes = [
     {
-        path: '/dashboard',
-        name: 'clients.index',
-        component: ClientsIndex
-    },
-    {
-        path: '/clients/create',
-        name: 'clients.create',
-        component: ClientsForm
-    },
-    {
-        path: '/clients/:id/update',
-        name: 'clients.update',
-        component: ClientsForm,
-        props: true
-    },
-    {
-        path: '/users',
-        name: 'users.index',
-        component: UsersIndex
-    },
-    {
-        path: '/users/create',
-        name: 'users.create',
-        component: CreateUsersForm
-    },
-    {
-        path: '/users/:id/update',
-        name: 'users.update',
-        component: UpdateUsersForm,
-        props: true
-    },
-    {
-        path: '/settings',
-        name: 'settings.index',
-        component: SettingsIndex,
-    },
-    {
-        path: '/settings/create',
-        name: 'settings.create',
-        component: CreateSettingsForm
-    },
-    {
-        path: '/settings/:id/update',
-        name: 'settings.update',
-        component: UpdateSettingsForm,
-        props: true
+        path: "/",
+        name: "layout",
+        component: Layout,
+        children: [
+            {
+                path: "/dashboard",
+                name: "clients.index",
+                component: ClientsIndex,
+            },
+            {
+                path: "/clients/create",
+                name: "clients.create",
+                component: ClientsForm,
+            },
+            {
+                path: "/clients/:id/update",
+                name: "clients.update",
+                component: ClientsForm,
+                props: true,
+            },
+            {
+                path: "/users",
+                name: "users.index",
+                component: UsersIndex,
+            },
+            {
+                path: "/users/create",
+                name: "users.create",
+                component: CreateUsersForm,
+            },
+            {
+                path: "/users/:id/update",
+                name: "users.update",
+                component: UpdateUsersForm,
+                props: true,
+            },
+            {
+                path: "/settings",
+                name: "settings.index",
+                component: SettingsIndex,
+            },
+            {
+                path: "/settings/create",
+                name: "settings.create",
+                component: CreateSettingsForm,
+            },
+            {
+                path: "/settings/:id/update",
+                name: "settings.update",
+                component: UpdateSettingsForm,
+                props: true,
+            },
+        ],
     },
 ];
 
 export default createRouter({
     history: createWebHistory(),
-    routes
-})
+    routes,
+});
