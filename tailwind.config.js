4cbade42ea97388958af8eadad260b1372e8d801
48969167 <mail.eduard.schwarzkopf@gmail.com>
2022-04-10 12:45:41 +0200

    added theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,36 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.vue',
-        
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+        "./resources/js/**/*.vue",
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+                sans: ["Nunito", ...defaultTheme.fontFamily.sans],
             },
         },
     },
-
     plugins: [require("daisyui")],
-    
+    daisyui: {
+        themes: [
+            {
+                mytheme: {
+                    primary: "#570DF8",
+                    secondary: "#F000B8",
+                    accent: "#37CDBE",
+                    neutral: "#3D4451",
+                    "base-100": "#FFFFFF",
+                    info: "#3ABFF8",
+                    success: "#36D399",
+                    warning: "#FBBD23",
+                    error: "#F87272",
+                },
+            },
+        ],
+    },
 };
