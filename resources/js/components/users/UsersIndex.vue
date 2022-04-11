95ff7c403941a9364e502b122153233085bccc2b
48969167 <mail.eduard.schwarzkopf@gmail.com>
2022-04-11 18:48:37 +0200

    updated view

--- resources/js/components/users/UsersIndex.vue
+++ resources/js/components/users/UsersIndex.vue
@@ -1,87 +1,54 @@
 <template>
     <div class="flex place-content-end mb-4">
-        <div
-            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        <router-link :to="{ name: 'users.create' }" class="btn btn-primary"
+            >Create user</router-link
         >
-            <router-link
-                :to="{ name: 'users.create' }"
-                class="text-sm font-medium"
-                >Create user</router-link
-            >
-        </div>
     </div>
 
-    <div
-        class="overflow-hidden overflow-x-auto min-w-full align-middle sm:rounded-md"
-    >
-        <table class="min-w-full border divide-y divide-gray-200">
+    <div class="overflow-x-auto">
+        <table class="table table-zebra w-full text-center">
             <thead>
                 <tr>
-                    <th class="px-6 py-3 bg-gray-50">
-                        <span
-                            class="text-xs font-medium tracking-wider leading-4 text-left text-gray-500 uppercase"
-                        >
-                            Name
-                        </span>
-                    </th>
-                    <th class="px-6 py-3 bg-gray-50">
-                        <span
-                            class="text-xs font-medium tracking-wider leading-4 text-left text-gray-500 uppercase"
-                        >
-                            E-Mail
-                        </span>
-                    </th>
-                    <th class="px-6 py-3 bg-gray-50">
-                        <span
-                            class="text-xs font-medium tracking-wider leading-4 text-left text-gray-500 uppercase"
-                        >
-                            Admin
-                        </span>
-                    </th>
-                    <th class="bg-gray-50"></th>
+                    <th>Id</th>
+                    <th>Name</th>
+                    <th>E-Mail</th>
+                    <th>Admin</th>
+                    <th></th>
                 </tr>
             </thead>
-
-            <tbody class="bg-white divide-y divide-gray-200 divide-solid">
-                <template v-for="item in users" :key="item.id">
-                    <tr class="bg-white">
-                        <td
-                            class="px-6 py-4 text-sm leading-5 text-gray-900 whitespace-no-wrap"
+            <tbody>
+                <tr v-for="item in users" :key="item.id">
+                    <td>
+                        {{ item.id }}
+                    </td>
+                    <td>
+                        {{ item.name }}
+                    </td>
+                    <td>
+                        {{ item.email }}
+                    </td>
+                    <td>
+                        {{ item.is_admin }}
+                    </td>
+                    <td>
+                        <router-link
+                            :to="{
+                                name: 'users.update',
+                                params: { id: item.id },
+                            }"
+                            class="mr-2 btn btn-sm"
                         >
-                            {{ item.name }}
-                        </td>
-                        <td
-                            class="px-6 py-4 text-sm leading-5 text-gray-900 whitespace-no-wrap"
+                            Edit</router-link
                         >
-                            {{ item.email }}
-                        </td>
-                        <td
-                            class="px-6 py-4 text-sm leading-5 text-gray-900 whitespace-no-wrap"
+                        <button
+                            @click="deleteClient(item.id)"
+                            v-if="item.id != currentUser.id"
+                            class="btn btn-outline btn-error btn-sm"
                         >
-                            {{ item.is_admin }}
-                        </td>
-                        <td
-                            class="px-6 py-4 text-sm text-right leading-5 text-gray-900 whitespace-no-wrap"
-                        >
-                            <router-link
-                                :to="{
-                                    name: 'users.update',
-                                    params: { id: item.id },
-                                }"
-                                class="mr-2 inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150"
-                            >
-                                Edit</router-link
-                            >
-                            <button
-                                @click="deleteUser(item.id)"
-                                v-if="item.id != currentUser.id"
-                                class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150"
-                            >
-                                Delete
-                            </button>
-                        </td>
-                    </tr>
-                </template>
+                            Delete
+                        </button>
+                    </td>
+                </tr>
             </tbody>
         </table>
     </div>
