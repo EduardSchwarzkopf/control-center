b55f6e4ec8a48476bee6116ea29120b2caa2a859
48969167 <mail.eduard.schwarzkopf@gmail.com>
2022-04-12 21:51:44 +0200

    refactored

--- resources/js/components/clients/ClientsIndex.vue
+++ resources/js/components/clients/ClientsIndex.vue
@@ -1,88 +1,60 @@
 <template>
-    <div class="flex place-content-end mb-4">
-        <router-link class="btn btn-primary" :to="{ name: 'clients.create' }"
-            >Create client</router-link
-        >
-    </div>
-    <div class="overflow-x-auto">
-        <table class="table table-zebra w-full text-center">
-            <thead>
-                <tr>
-                    <th>Id</th>
-                    <th>Name</th>
-                    <th>URL</th>
-                    <th>Active</th>
-                    <th></th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="item in clients" :key="item.id">
-                    <td>
-                        {{ item.id }}
-                    </td>
-                    <td>
-                        {{ item.name }}
-                    </td>
-                    <td>
-                        {{ item.url }}
-                    </td>
-                    <td>
-                        {{ item.is_active }}
-                    </td>
-                    <td>
+    <div class="">
+        <div class="grid grid-cols-3 gap-3">
+            <div class="col-span-1 p-6 bg-white rounded">
+                <div class="flex place-content-end mb-4">
+                    <router-link
+                        class="btn btn-primary"
+                        :to="{ name: 'clients.create' }"
+                        >Add client</router-link
+                    >
+                </div>
+                <div class="space-y-6">
+                    <div
+                        class="w-full hover:bg-gray-100"
+                        :class="
+                            state.cliendId == item.id ? 'bg-primary-100' : ''
+                        "
+                        v-for="item in clients"
+                        :key="item.id"
+                    >
                         <router-link
                             :to="{
                                 name: 'client.show',
                                 params: { id: item.id },
                             }"
-                            class="mr-2 btn btn-primary btn-sm"
+                            @click="setClientId(item.id)"
+                            :class="` rounded px-2 py-4 mb-4 w-full ${
+                                item.is_active ? 'opacity-50' : ''
+                            }`"
                         >
-                            Show</router-link
-                        >
-                        <router-link
-                            :to="{
-                                name: 'clients.update',
-                                params: { id: item.id },
-                            }"
-                            class="mr-2 btn btn-sm"
-                        >
-                            Edit</router-link
-                        >
-                        <button
-                            @click="deleteClient(item.id)"
-                            class="btn btn-outline btn-error btn-sm"
-                        >
-                            Delete
-                        </button>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+                            {{ item.name }}
+                        </router-link>
+                    </div>
+                </div>
+            </div>
+            <div class=""><router-view :key="state.cliendId" /></div>
+        </div>
     </div>
 </template>
 
 <script>
 import useClients from "../../composables/clients";
-import { onMounted } from "vue";
+import { reactive } from "vue";
 
 export default {
+    methods: {
+        setClientId(id) {
+            this.state.cliendId = id;
+        },
+    },
     setup() {
-        const { clients, getClients, destroyClient } = useClients();
-
-        const deleteClient = async (id) => {
-            if (!window.confirm("You sure?")) {
-                return;
-            }
-
-            await destroyClient(id);
-            await getClients();
-        };
-
-        onMounted(getClients);
-
+        const { clients, getClients } = useClients();
+        const state = reactive({ cliendId: 0 });
+        getClients();
         return {
             clients,
-            deleteClient,
+            state,
         };
     },
 };
