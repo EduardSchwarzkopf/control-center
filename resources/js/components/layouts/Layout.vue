a4d495c2652b63a5f9c7f42d69d7021a07ef6149
48969167 <mail.eduard.schwarzkopf@gmail.com>
2022-04-12 21:51:54 +0200

    style changed

--- resources/js/components/layouts/Layout.vue
+++ resources/js/components/layouts/Layout.vue
@@ -3,11 +3,9 @@
         <navigation :currentUser="currentUser" />
         <div id="app-layout">
             <div class="py-12">
-                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div
-                        class="bg-white overflow-hidden shadow-sm sm:rounded-lg"
-                    >
-                        <div class="p-6 bg-white border-b border-gray-200">
+                <div class="grid grid-cols-1 mx-auto sm:px-6 lg:px-8">
+                    <div class="overflow-hidden sm:rounded-lg">
+                        <div class="">
                             <router-view />
                         </div>
                     </div>
